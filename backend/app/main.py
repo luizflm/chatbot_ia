313f6ed015f623eae033966ddf816797e850dba3
Luiz Felipe <lepinholima@gmail.com>
2026-03-12 11:27:18 -0300

    feat: add short-term memory with message summarization

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,17 +8,41 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.agents.middleware import dynamic_prompt, ModelRequest
 from langchain.agents import create_agent
-from langchain.messages import SystemMessage, HumanMessage
+from langchain.messages import HumanMessage
 from langchain_postgres import PGVector
+from langgraph.checkpoint.postgres import PostgresSaver
+from langchain.agents.middleware import SummarizationMiddleware
+from contextlib import asynccontextmanager
 from tempfile import NamedTemporaryFile
 
-app = FastAPI()
+POSTGRES_CONNECTION_STRING = (
+    f"postgresql://"
+    f"{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
+    f"@{os.getenv('POSTGRES_HOST', 'postgres')}:{os.getenv('POSTGRES_PORT', '5432')}"
+    f"/{os.getenv('POSTGRES_DB')}"
+)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+PGVECTOR_CONNECTION_STRING = (
+    f"postgresql+psycopg://"
+    f"{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
+    f"@{os.getenv('POSTGRES_HOST', 'postgres')}:{os.getenv('POSTGRES_PORT', '5432')}"
+    f"/{os.getenv('POSTGRES_DB')}"
+)
+
+
+model = ChatOpenAI(model="gpt-4.1")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+vector_store = PGVector(
+    embeddings=embeddings,
+    collection_name="docs",
+    connection=PGVECTOR_CONNECTION_STRING,
+)
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    add_start_index=True,
 )
 
 
@@ -26,26 +50,33 @@ class ChatResponse(BaseModel):
     answer: str
 
 
-model = ChatOpenAI(model="gpt-4.1")
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    with PostgresSaver.from_conn_string(POSTGRES_CONNECTION_STRING) as checkpointer:
+        checkpointer.setup()
 
-CONNECTION_STRING = (
-    f"postgresql+psycopg://"
-    f"{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
-    f"@{os.getenv('POSTGRES_HOST', 'postgres')}:{os.getenv('POSTGRES_PORT', '5432')}"
-    f"/{os.getenv('POSTGRES_DB')}"
-)
+        app.state.agent = create_agent(
+            model,
+            tools=[],
+            checkpointer=checkpointer,
+            middleware=[
+                SummarizationMiddleware(
+                    model="gpt-4.1",
+                    trigger=("tokens", 1000),
+                    keep=("messages", 10)
+                ),
+                prompt_with_context
+            ]
+        )
+        yield
 
-vector_store = PGVector(
-    embeddings=embeddings,
-    collection_name="docs",
-    connection=CONNECTION_STRING,
-)
+app = FastAPI(lifespan=lifespan)
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200,
-    add_start_index=True,
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
@@ -63,14 +94,13 @@ def prompt_with_context(request: ModelRequest) -> str:
     )
 
 
-agent = create_agent(model, tools=[], middleware=[prompt_with_context])
-
-
 @app.post("/query", response_model=ChatResponse)
 async def query(
     message: str = Form(),
     file: UploadFile | None = None
 ):
+    agent = app.state.agent
+
     if file:
         if file.content_type != "application/pdf":
             raise HTTPException(400, "Only PDF files are supported.")
@@ -88,17 +118,11 @@ async def query(
         finally:
             os.remove(tmp_path)
 
-        result = agent.invoke({
-            "messages": [
-                HumanMessage(content=message)
-            ]
-        })
+    result = agent.invoke(
+        {"messages": [HumanMessage(content=message)]},
+        {"configurable": {"thread_id": "1"}},
+    )
 
-        response = result["messages"][-1]
-    else:
-        response = model.invoke([
-            SystemMessage("You are a helpful assistant."),
-            HumanMessage(content=message)
-        ])
+    response = result["messages"][-1]
 
     return ChatResponse(answer=response.content)
